d6b4c1a888d5f6ac8402990851e85aa937a2d4cf
Néstor <nestor.amichetti@gmail.com>
2021-02-03 02:37:10 -0300

    Finalizando Spring boot para heroku

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,13 @@
 /* Populate tabla clientes */
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andrés', 'Guzmán', 'profesor@bolsadeideas.com', '2018-01-01');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Mr. John', 'Doe', 'john.doe@gmail.com', '2018-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2018-01-03');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2018-01-04');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Erich', 'Gamma', 'erich.gamma@gmail.com', '2018-02-01');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Richard', 'Helm', 'richard.helm@gmail.com', '2018-02-10');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2018-02-18');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('John', 'Vlissides', 'john.vlissides@gmail.com', '2018-02-28');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Dr. James', 'Gosling', 'james.gosling@gmail.com', '2018-03-03');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Magma', 'Lee', 'magma.lee@gmail.com', '2018-03-04');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Tornado', 'Roe', 'tornado.roe@gmail.com', '2018-03-05');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jade', 'Doe', 'jane.doe@gmail.com', '2018-03-06');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Cristiano', 'Ronaldo', 'Ronaldo@hotmail.com', '2021-11-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Lionel', 'Messi', 'lio@gmail.com', '2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Amdrés', 'Iniesta', 'iniesta@gmail.com', '2018-01-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Sergio', 'Ramos', 'ramos@gmail.com', '2018-11-14');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Eden', 'Hazard', 'hazard@gmail.com', '2018-02-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Nestor', 'Amichetti', 'nestor@gmail.com', '2018-02-10');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Roy', 'Kane', 'roy@gmail.com', '2020-02-08');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Maxi', 'Rodriguez', 'maxi@gmail.com', '2008-12-28');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Xavi', 'Hernández', 'xavi@gmail.com', '2020-03-22');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Frank', 'Lampard', 'lampard@gmail.com', '2018-03-04');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ronaldinho', 'Gaucho', 'ronaldinho@gmail.com', '2005-03-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Sadio', 'Mané', 'mane@gmail.com', '2018-03-06');
